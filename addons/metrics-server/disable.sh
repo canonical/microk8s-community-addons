631b2f971458d32a058ec21b1c51a2295e624bd1
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-01-30 14:42:53 +0200

    Fix metrics-server

--- addons/metrics-server/disable.sh
+++ addons/metrics-server/disable.sh
@@ -7,10 +7,10 @@ source $SNAP/actions/common/utils.sh
 echo "Disabling Metrics-Server"
 
 KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
-# Clean up old metrics-server  
+# Clean up old metrics-server
 $KUBECTL delete configmap -n kube-system metrics-server-config || true
-$KUBECTL delete deployment -n kube-system metrics-server-v0.2.1 || true 
+$KUBECTL delete deployment -n kube-system metrics-server-v0.2.1 || true
 
-use_manifest metrics-server delete 
+use_manifest metrics-server/metrics-server delete
 
 echo "Metrics-Server is disabled"
--- addons/metrics-server/enable.sh
+++ addons/metrics-server/enable.sh
@@ -7,7 +7,7 @@ source $SNAP/actions/common/utils.sh
 echo "Enabling Metrics-Server"
 
 KUBECTL="$SNAP/kubectl --kubeconfig=${SNAP_DATA}/credentials/client.config"
-use_manifest metrics-server apply
+use_manifest metrics-server/metrics-server apply
 
 refresh_opt_in_config "authentication-token-webhook" "true" kubelet
 restart_service kubelet
