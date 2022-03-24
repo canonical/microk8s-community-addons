e3bc44ad21e4347d82ee89fc319217de83ce0433
balchua <balchua@yahoo.com>
2022-03-24 17:21:26 +0800

    Fix linkerd testing (#31)

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -213,11 +213,11 @@ class TestAddons(object):
         microk8s_disable("cilium")
         microk8s_reset()
 
-    @pytest.mark.skip("disabling the test while we work on a 1.20 release")
     @pytest.mark.skipif(
         os.environ.get("UNDER_TIME_PRESSURE") == "True",
         reason="Skipping Linkerd tests as we are under time pressure",
     )
+    @pytest.mark.skipif(platform.machine() == "s390x", reason="Not available on s390x")
     def test_linkerd(self):
         """
         Sets up and validate linkerd
