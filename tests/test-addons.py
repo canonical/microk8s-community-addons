73518eddbff8754fb3eec346c6734e72b9af44a2
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-01-10 19:56:00 +0200

    Lets run knative test on GH

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -173,10 +173,6 @@ class TestAddons(object):
         microk8s_disable("inaccel")
 
     @pytest.mark.skipif(platform.machine() == "s390x", reason="Not available on s390x")
-    @pytest.mark.skipif(
-        os.environ.get("UNDER_TIME_PRESSURE") == "True",
-        reason="Skipping knative tests as we are under time pressure",
-    )
     def test_knative(self):
         """
         Test knative
