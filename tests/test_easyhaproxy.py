19ac9312fafb4f6b63522edbf10cf0d0c40a0e5c
Joao Gilberto Magalhaes <joao@byjg.com.br>
2023-02-08 09:21:50 -0600

    Add EasyHAProxy addon

---
+++ tests/test_easyhaproxy.py
@@ -0,0 +1,30 @@
+import pytest
+import platform
+
+from utils import (
+    microk8s_disable,
+    microk8s_enable,
+    wait_for_pod_state,
+)
+
+
+class TestEasyHAProxy(object):
+    @pytest.mark.skipif(platform.machine() == "s390x", reason="Not available on s390x")
+    def test_easyhaproxy(self):
+        """
+        Sets up and validates easyhaproxy.
+        """
+        print("Enabling easyhaproxy")
+        microk8s_enable("easyhaproxy")
+        print("Validating easyhaproxy")
+        self.validate_easyhaproxy()
+        print("Disabling easyhaproxy")
+        microk8s_disable("easyhaproxy")
+
+    def validate_easyhaproxy(self):
+        """
+        Validate easyhaproxy
+        """
+        wait_for_pod_state(
+            "", "easyhaproxy", "running", label="app.kubernetes.io/name=easyhaproxy"
+        )
