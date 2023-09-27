9cc6cf4c24e361e4d55b78b852c20221538e562e
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-09-27 09:19:28 +0300

    Skip kwasm testing until version is pinned

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -500,6 +500,9 @@ class TestAddons(object):
         microk8s_disable("sosivio")
 
     @pytest.mark.skipif(platform.machine() == "s390x", reason="Not available on s390x")
+    @pytest.mark.skip(
+        reason="See https://github.com/canonical/microk8s-community-addons/issues/180"
+    )
     def test_kwasm(self):
         """
         Sets up and validates kwasm.
