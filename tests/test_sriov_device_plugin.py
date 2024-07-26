757f5e45b9c1ae14629c4f89e4cd97b9007b4203
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2024-07-26 19:15:26 +0400

    Fix tests

--- tests/test_sriov_device_plugin.py
+++ tests/test_sriov_device_plugin.py
@@ -1,13 +1,19 @@
 import os
 import unittest
-import sys
-from unittest.mock import patch
+import json
 import pytest
 import platform
+from importlib.util import spec_from_loader, module_from_spec
+from importlib.machinery import SourceFileLoader 
+import pathlib
+from unittest import mock
 
-from subprocess import PIPE, STDOUT, CalledProcessError, run
+from subprocess import CalledProcessError, run
 from utils import microk8s_disable
 
+spec = spec_from_loader("enable", SourceFileLoader("enable", str( pathlib.Path(os.path.dirname(__file__)).parent.absolute() / "addons" / "sriov-device-plugin" / "enable")))
+enable = module_from_spec(spec)
+spec.loader.exec_module(enable)
 
 class TestSRIOVDevicePlugin(unittest.TestCase):
     """SR-IOV Network Device Plugin relies on availability of given PCI devices, so we can only
@@ -52,13 +58,13 @@ class TestSRIOVDevicePlugin(unittest.TestCase):
         microk8s_disable("sriov-device-plugin")
 
     def mock_check_output(self, command, text=True):
-        KUBECTL = os.path.expandvars("$SNAP/microk8s-kubectl.wrapper")
+        # KUBECTL = os.path.expandvars("$SNAP/microk8s-kubectl.wrapper")
 
         if command == ["lspci", "-s", "0000:00:06.0"]:
             return "something"
         elif command == ["lspci", "-s", "0000:00:07.0"]:
             return "something"
-        elif command == [KUBECTL, "get", "node", "-o", "json"]:
+        elif command == ["sudo", "microk8s", "kubectl", "get", "node", "-o", "json"]:
             return """{
    "items": [
       {
@@ -73,33 +79,26 @@ class TestSRIOVDevicePlugin(unittest.TestCase):
 }"""
         raise ValueError(f"Unmocked command: {command}")
 
-    @patch("subprocess.check_output")
-    def test_sriov_device_plugin_works_correctly(self, mock_check_output):
+    def test_sriov_device_plugin_works_correctly(self):
         """
         Make sure plugin enables and disables successfully.
         """
-        mock_check_output.side_effect = self.mock_check_output
         script_path = os.path.abspath(os.path.dirname(__file__))
-        sriov_resources_mapping_file_name = (
+        resources_file_name = (
             "sriov-device-plugin-test-resources-valid.json"
         )
-        sriov_resources_mapping_file = os.path.join(
+        resource_file = os.path.join(
             script_path,
             "resources",
-            sriov_resources_mapping_file_name,
+            resources_file_name
         )
-        enable_sriov_dp_cmd = [
-            "/snap/bin/microk8s.enable",
-            "sriov-device-plugin",
-            "--resources-file",
-            sriov_resources_mapping_file,
-        ]
         print("Enabling SR-IOV Network Device Plugin")
 
-        run(
-            enable_sriov_dp_cmd,
-            check=True,
-        )
+        with open(resource_file, "r") as f:
+            resources = json.load(f)
+            with mock.patch("subprocess.check_output") as mocked_subprocess:
+                mocked_subprocess.side_effect = self.mock_check_output
+                enable.main(testing=True, resources=resources) 
 
         print("Disabling SR-IOV Network Device Plugin")
         microk8s_disable("sriov-device-plugin")
