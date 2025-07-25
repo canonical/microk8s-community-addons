65b23447d24d1d827307591ba625407c5bb067f7
Alice Lilith <lilith.alice@protonmail.com>
2025-07-25 14:54:47 -0700

    update ngrok addon
    Signed-off-by: Alice Lilith <lilith.alice@protonmail.com>

--- tests/test_ngrok.py
+++ tests/test_ngrok.py
@@ -10,7 +10,6 @@ from utils import (
 
 
 class TestNgrok(object):
-    @pytest.mark.skip(reason="Ngrok ingress controller is depreciated.")
     @pytest.mark.skipif(platform.machine() == "s390x", reason="Not available on s390x")
     @pytest.mark.skipif(
         os.environ.get("STRICT") == "yes",
@@ -22,23 +21,23 @@ class TestNgrok(object):
     )
     def test_ngrok(self):
         """
-        Sets up and validates ngrok.
+        Sets up and validates the ngrok kubernetes operator.
         """
-        print("Enabling ngrok")
+        print("Enabling ngrok kubernetes operator")
         microk8s_enable(
             addon="ngrok",
-            optional_args="--namespace ngrok-ingress-controller --secret-name test",
+            optional_args="--namespace ngrok-operator --secret-name test",
         )
-        print("Validating ngrok")
+        print("Validating ngrok kubernetes operator")
         self.validate_ngrok()
-        print("Disabling ngrok")
+        print("Disabling ngrok kubernetes operator")
         microk8s_disable("ngrok")
 
     def validate_ngrok(self):
         """
-        Validate ngrok
+        Validate ngrok kubernetes operator
         """
         kubectl_get(
-            "deployment ngrok-ingress-controller-kubernetes-ingress-controller-manager"
-            " -n ngrok-ingress-controller"
+            "deployment ngrok-operator-manager"
+            " -n ngrok-operator"
         )
