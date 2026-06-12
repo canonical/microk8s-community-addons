ec055415aba896ea8c6a09baf773af6de5971d47
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2026-06-12 11:00:55 +0300

    Find the sideffect

--- tests/test_openebs.py
+++ tests/test_openebs.py
@@ -17,7 +17,7 @@ class TestOpenebs(object):
         platform.machine() == "s390x", reason="OpenEBS is not available on s390x"
     )
     @pytest.mark.skipif(
-        os.environ.get("UNDER_TIME_PRESSURE") == "True",
+        os.environ.get("UNDER_TIME_PRESSURE") == None,
         reason="Skipping test, expected to be tested when under time pressure",
     )
     def test_openebs(self):
